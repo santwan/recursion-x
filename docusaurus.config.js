40999531188a9a6b2733a56c9188a00bbda455fd
santwan <santwanpathak1@gmail.com>
2026-04-03 20:29:16 +0530

    Update baseUrl for Vercel

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://santwan.github.io',
+  url: 'https://recursion-x.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/recursion-x/',
+  // For Vercel deployment, use '/'
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
